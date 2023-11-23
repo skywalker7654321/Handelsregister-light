40329905ed8ee8aa7355b1f7baef15dc62ecc778
B. Dong <139446816+skywalker7654321@users.noreply.github.com>
2023-11-23 23:46:47 +0100

    Finish persoongegevens html/css

--- src/app/persoonsgegevens/persoonsgegevens.component.ts
+++ src/app/persoonsgegevens/persoonsgegevens.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./persoonsgegevens.component.css']
 })
 export class PersoonsgegevensComponent {
+onInputLand(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputPostcode(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputToevoegingHuisnummer(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputHuisnummer(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputStraatnaam(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputGeboorteDatum(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputGeslacht(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputAchternaam(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputTussenvoegsel(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputVoornaam(arg0: string) {
+throw new Error('Method not implemented.');
+}
+onInputBedrijfsnaam(arg0: string) {
+throw new Error('Method not implemented.');
+}
 
 }
